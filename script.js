f1ddfbfd72c3ac6bc3efbec8ad1cd83afc5a48c5
Laura Santos <alsr16p@gmail.com>
2021-08-26 16:54:56 +0100

    script working

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
+//Selectors
+
 let add = document.querySelector("#add"); //to target the button
 let list = document.querySelector("#list"); //to add to the list the chores
 
-//function to make the button work and add what is written
-add.onclick = event => {
-    event.preventDefault();
+//Event Listeners
+
+add.onclick = (e) => {
+    e.preventDefault();
 
     let listitem = document.querySelector("#listitem");
 
-    //in case of no type
+    if(listitem.value !== '') { //for when we write something
+        let el = document.createElement("li"); //target the list
+        el.innerHTML = listitem.value;
 
-    if (!listitem.value.length) {
-        return alert("We could not find any word to add");
+        let removebtn = document.createElement("button");
+        removebtn.className = "close";
+        removebtn.innerHTML = "\u00D7"; //is the unicode for the "X" symbol
+
+        el.appendChild(removebtn);
+        list.appendChild(el);
+        listitem.value = "";
+
+        el.addEventListener("click", (e) => {
+            e.target.style.textDecoration = "line-through"; 
+        })
+        removebtn.addEventListener("click", (e) => {
+            e.target.parentElement.style.display = "none";
+        });
+    } else {
+        alert("We could not find any word to add");
     }
-
-    let li = document.createElement("li");
-    let button = document.createElement("button");
-
-    li.innerHTML = listitem.value;
-
-    button.className = "close";
-    button.innerHTML = "\u00D7";
-
-    li.appendChild(button);
-    listitem.appendChild(li);
-
-    listitem.value = ""; //default
-
-    li.onclick = event => {
-        event.target.style.textDecoration = "line-through";
-    };
-
-    button.onclick = event => {
-        event.target.parentElement.style.display = "none";
-    };
 }
